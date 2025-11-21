fa50b438e757623579e766629d848d359a844a22
asroralva <asroralvaizzi191919@gmail.com>
2025-11-21 19:30:13 +0700

    fix: deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import HomePage from "./pages/HomePage";
 import ProjectsPage from "./pages/ProjectsPage";
 import { Toaster } from "./components/ui/toaster";
 import ProjectDetail from "./pages/ProjectDetail";
-import NotFound from "./pages/notfound";
+import NotFound from "./pages/NotFound";
 import Resume from "./pages/resume";
 import Blog from "./pages/Blog";
 import Cheetseet from "./pages/Cheetseet";
